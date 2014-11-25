97dd0b33a2c93ab6ccf4e8eddc662ebb7f793903
JonathanMurray <jonte_murray@hotmail.com>
2014-11-25 23:45:59 +0100

    Added javadoc description of Visualizer interface.

--- Visualizer.java
+++ Visualizer.java
@@ -1,4 +1,12 @@
-
+/**
+ * This class is used from within algorithms, to visualize the running of the algorithm.
+ * setPath must be called as soon as the first has been generated. Then the reference is kept by
+ * the visualizer. repaint() must be called everytime the path changes if changes should be drawn.
+ * highlight() can be used to further improve the visualization, and sleep can be used to control
+ * running speed.
+ * @author Jonathan
+ *
+ */
 public interface Visualizer {
 	//Empty default methods to get a "mockup" for free
 	default void repaint(){}
