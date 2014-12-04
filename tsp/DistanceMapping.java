134ac1bc20fb2ffd8f3302d8d1c334bbb652ed7e
JonathanMurray <jonte_murray@hotmail.com>
2014-12-04 15:20:20 +0100

    Changed to real distance from sqDistance

--- tsp/DistanceMapping.java
+++ tsp/DistanceMapping.java
@@ -2,13 +2,20 @@ package tsp;
 
 public class DistanceMapping {
 
+	
+	/**
+	 * Creates a matrix where m[i,j] is the distance from nodes[i] to nodes[j]
+	 * Use it like this: m[path[aIndex]][path[bIndex]]
+	 * @param nodes
+	 * @return
+	 */
 	public static float[][] getDistances(Node[] nodes){
 //		System.out.println("dist");
 		float[][] distances = new float[nodes.length][];
 		for(int i = 0; i < nodes.length; i++){
 			distances[i] = new float[nodes.length];
 			for(int j = 0; j < nodes.length; j++){
-				distances[i][j] = (float)nodes[i].sqDistance(nodes[j]);
+				distances[i][j] = (float)nodes[i].distance(nodes[j]);
 			}
 		}
 //		System.out.println("done");
